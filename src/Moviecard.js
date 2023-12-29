9b2acfc5fa35a11b061587a8ed55e122a4ce38f2
nain333 <hnain70@gmail.com>
2023-12-29 15:26:26 +0530

    add stars to component

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -3,7 +3,7 @@ class MovieCard extends Component {
   render () {
     return (<div className='main'>
       <div className='movie-card'>
-        <div className='left'> <img alt="Poster"/> </div>
+        <div className='left'> <img alt="Poster" src='https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/71wwUV0wY8L._AC_UF1000,1000_QL80_.jpg'/> </div>
 
         <div className='right'>
             <div className='title'>The Avangers</div>
@@ -11,7 +11,12 @@ class MovieCard extends Component {
             <div className='price'> Rs. 199 </div>
             <div className='footer'>
             <div className='rating'>8.9</div>
-            <div className='stars'>stars</div>
+            <div className='star-dis'>
+                <img className='str-btn' alt='decrease' src='https://cdn-icons-png.flaticon.com/128/56/56889.png'></img>
+                <img className='stars' src="https://cdn-icons-png.flaticon.com/128/616/616490.png"></img>
+                <img className='str-btn' alt='increase' src='https://cdn-icons-png.flaticon.com/128/1237/1237946.png'></img>
+                <span>0</span>
+                </div>
             <button className='favourite-btn'>
                 Faviorate
             </button>
