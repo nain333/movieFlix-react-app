60267085f822e2e77c90b3f8933947993497dabd
nain333 <hnain70@gmail.com>
2024-01-05 19:16:55 +0530

    add Dynamic styles using props

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import './index.css';
 import styled from "styled-components"
 
 const Nav =  styled.div`
@@ -19,6 +18,8 @@ const Title = styled.div`
     font-family: 'Times New Roman', Times, serif;
     text-transform: uppercase;
     margin-left: 20px ;
+    &:hover{color:#f00};
+
 `;
 
 
@@ -33,13 +34,14 @@ const CartIconContainer = styled.div`
 `;
 
 const CartCount = styled.div`
-    background: yellow;
+    background: ${props => props.color};
     border-radius: 50%;
     padding: 4px 8px;
     position: absolute;
     right: 10px;
     top: -5px;
     font-size: 12px;
+    visibility: ${props => (props.show? "visible": "hidden")};
 `;
 
 
@@ -59,7 +61,7 @@ class Navbar extends React.Component {
             <CartImg alt="Cart-Icon" 
                  src = "https://cdn-icons-png.flaticon.com/128/891/891462.png"    
             />
-            <CartCount >5</CartCount>
+            <CartCount color="yellow" show={true}>5</CartCount>
         </CartIconContainer>
       </Nav>
 
